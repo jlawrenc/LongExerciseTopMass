30778c79eba3a40e7b7e1d0747c26bdc3a2bbfbb
hirakban <hirakban@buffalo.edu>
2020-01-16 21:00:40 -0600

    Pull mean and pull width are updated

--- fitNcalibrate/fitPeak.py
+++ fitNcalibrate/fitPeak.py
@@ -228,10 +228,10 @@ def plotter_cal(h=None,name=None):
     gROOT.ForceStyle()
     gROOT.Reset()
     h.UseCurrentStyle()
-    fitfunc_gaus = TF1("Gaussian", myFitFunc, 64.0, 67.0, 3)
+    fitfunc_gaus = TF1("Gaussian", myFitFunc, 65.0, 69.0, 3)
     ## Set normalization
     fitfunc_gaus.SetParameter(0, h.Integral());
-    fitfunc_gaus.SetParLimits(0, 0.1*h.Integral(), 2.5*h.Integral());
+    fitfunc_gaus.SetParLimits(0, 0.1*h.Integral(), 2.0*h.Integral());
     ## Set gaussian mean starting value and limits
     fitfunc_gaus.SetParameter(1, 67.5);
     fitfunc_gaus.SetParLimits(1, 64.0, 70.0);
@@ -268,12 +268,6 @@ def plotter_cal(h=None,name=None):
     c1.SaveAs(name)
     c1.Close()
     del c1
-
-
-
-
-
-
 
 
 
@@ -316,6 +310,42 @@ def plotter_err(h=None,name=None):
 
 
 
+def plotter_cal_err(h=None,name=None):
+    c1 = TCanvas("c1","")
+    c1.cd()
+    tdrstyle.setTDRStyle()
+    gROOT.ForceStyle()
+    gROOT.Reset()
+    h.UseCurrentStyle()
+    h.Fit("gaus","","",0.1,0.9)
+    h.Draw()
+
+    label1 = TLatex()
+    label1.SetNDC()
+    label1.SetTextFont(60)
+    label1.SetTextSize(0.07)
+    label1.SetTextAlign(31)
+    label1.DrawLatex(0.32, 0.92, "CMS DAS")
+    label2 = TLatex()
+    label2.SetNDC()
+    label2.SetTextFont(42)
+    label2.SetTextSize(0.06)
+    label2.SetTextAlign(11)
+    label2.DrawLatex(0.33, 0.92, "#it{Simulation}")
+
+    c1.Update()
+    stats = c1.GetPrimitive("stats")
+    stats.__class__ = ROOT.TPaveStats
+    stats.SetY1NDC(0.6)
+    stats.SetY2NDC(0.9)
+    stats.SetX1NDC(0.6)
+    stats.SetX2NDC(0.9)
+    c1.RedrawAxis()
+    c1.Update()
+
+    c1.SaveAs(name)
+    c1.Close()
+    del c1
 
 
 
@@ -389,12 +419,26 @@ def main():
     E_peak_err = TH1F("E_peak_err", "", 400,0.01,0.15) # 172v5
     #E_peak_err = TH1F("E_peak_err", "", 30,0.08,0.2) # 175v5
 
+    #delEb_calibrated = TH1F("delEb_calibrated", "", 240,0.2,1.0) # 169v5
+    delEb_calibrated = TH1F("delEb_calibrated", "", 80,0.2,1.0) # 172v5
+    #delEb_calibrated = TH1F("delEb_calibrated", "", 240,0.2,1.0) # 175v5
+
+
+
+
     hpull = TH1F("hpull", "",100,-100,100)
-#   hpullcal = TH1F("hpullcal", "",100,-100,100)
+    hpullcal = TH1F("hpullcal", "",100,-10,10)
 
     #pred = 65.740 #169v5
     pred = 67.57 #172v5
     #pred = 69.39 #175v5
+
+    p0 = 29.6936
+    p1 = 0.529809
+    delp0 = 6.57756
+    delp1 = 0.0972778
+    covar = 0.639692435
+    DEb_uncal = 0.257576
 
     for i in range(0,Npe):
         hpe = histo.Clone()
@@ -412,9 +456,20 @@ def main():
         E_peak_err.Fill(DEb)
 
         #apply the calibration
-        Eb_cal=(Eb-29.6936)/0.529809
+        Eb_cal=(Eb-p0)/p1
         Eb_calibrated.Fill(Eb_cal)
-   
+
+        DEb_cal = Eb_cal*( ( (DEb_uncal**2.0+delp0**2.0)/(Eb-p0)**2.0 ) + (delp1/p1)**2.0 - ((2.0*covar)/((Eb-p0)*p1))  )**(0.5)    
+        delEb_calibrated.Fill(DEb_cal)
+#        print DEb_cal
+
+        #calculating the calibrated pull
+        pullcal=(Eb_cal-pred)/DEb_cal
+        if abs(pullcal)<5.0:
+            hpullcal.Fill(pullcal)
+
+
+
 #        pull=(Eb_calibrated-pred)/DEb
 #        hpull.Fill(pull)
 #        print Eb, DEb, "in loop"
@@ -427,12 +482,16 @@ def main():
 #   pullcal=(Ebcal-pred)/DEbcal
 #   hpullcal.Fill(pullcal)
 
-#   plotter(E_peak,"Eb_m172p5.pdf")
+    plotter(E_peak,"Eb_m172p5.pdf")
     plotter_cal(Eb_calibrated,"Eb_cal_m172p5.pdf")
 
-#   plotter_err(E_peak_err,"Eb__m172p5_err.pdf")
+    plotter_err(E_peak_err,"Eb__m172p5_err.pdf")
+    plotter_cal_err(delEb_calibrated,"delEb_m172p5.pdf")
+
+
+
 #   plotter_err(hpull,"Pull_m172p5.pdf")
-#   plotter(hpullcal,"Pull_corr.pdf")
+    plotter_err(hpullcal,"Pull_corr_m172p5.pdf")
 
 
 
---
+++ fitNcalibrate/pullwidthplotter.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+import ROOT
+from ROOT import *
+from array import array
+import tdrstyle
+
+# Add a zero to the theoretical values to make a nice y=x plot
+theor=array('d',[0.0,65.74033,67.57093,69.390239, 75.0])
+exper=array('d',[0.2172,-0.3001,0.1302]) # Pseudo
+#exper=array('d',[64.89,66.34,67.09])
+theor_err=array('d',[1.0,1.0,1.0,1.0,1.0])
+exper_err=array('d',[0.7613,0.8152,0.6955]) # Pseudo
+#exper_err=array('d',[0.59,0.26,0.64])
+
+#for i in range(0,len(exper)):
+#    exper[i]=(exper[i]-16.51)/0.729
+#    exper_err[i]=(exper_err[i])/0.729
+
+c1 = TCanvas("canvas 1", "c1")
+c1.cd()
+tdrstyle.setTDRStyle()
+
+graph = TGraph(len(exper),theor[1:],exper_err)
+graph.SetMarkerColor(4)
+graph.SetMarkerStyle(21)
+graph.SetTitle("")
+graph.GetXaxis().SetTitle("Predicted Energy Peak Position [GeV]")
+graph.GetYaxis().SetTitle("Pull Width")
+graph.GetXaxis().SetRangeUser(60.0,75.0)
+graph.GetYaxis().SetRangeUser(0.0,2.0)
+graph.GetXaxis().SetTitleFont(42)
+graph.GetYaxis().SetTitleFont(42)
+graph.GetXaxis().SetTitleSize(0.035)
+graph.GetYaxis().SetTitleSize(0.035)
+graph.GetXaxis().SetTitleOffset(1.4)
+graph.GetYaxis().SetTitleOffset(1.4)
+
+
+graph.Draw("AP")
+
+ideal = TGraph(len(theor),theor,theor_err)
+ideal.SetLineColor(4)
+ideal.SetLineStyle(7)
+ideal.Draw("SAME")
+
+label1 = TLatex()
+label1.SetNDC()
+label1.SetTextFont(60)
+label1.SetTextSize(0.07)
+label1.SetTextAlign(31)
+label1.DrawLatex(0.32, 0.92, "CMS DAS")
+label2 = TLatex()
+label2.SetNDC()
+label2.SetTextFont(42)
+label2.SetTextSize(0.06)
+label2.SetTextAlign(11)
+label2.DrawLatex(0.33, 0.92, "#it{Simulation}")
+
+leg = TLegend(0.3,0.6,0.7,0.69)
+leg.AddEntry(graph,"Simulation","p")
+leg.AddEntry(ideal,"Expectation","l")
+#leg.AddEntry(gfit,"Fit","l")
+leg.Draw("SAME")
+
+gPad.Update()
+
+
+# Save the plot
+gPad.RedrawAxis()
+c1.SaveAs("pull_width_all.png")
